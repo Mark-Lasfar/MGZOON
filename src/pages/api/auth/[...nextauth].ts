9fcbf8f0376ce573655023f020e07824e10b656b
Mark EL-asfar <140356054+Mark-Lasfar@users.noreply.github.com>
2024-12-31 15:40:36 +0000

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,34 +1,3 @@
-// import NextAuth from "next-auth";
-// import GoogleProvider from "next-auth/providers/google";
-// import Facebook from "next-auth/providers/facebook"
- 
-
-
-
-
-// export default NextAuth({
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-//     }),
-    // Facebook({
-    //   clientId: process.env.FACEBOOK_CLIENT_ID!,
-    //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
-    // }),
-//   ],
-//   secret: process.env.NEXTAUTH_SECRET
-// });
-
-
-// export const { handlers, signIn, signOut, auth } = NextAuth({
-//   providers: [Facebook],
-// })
-
-
-
-
-// import NextAuth from "next-auth";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
